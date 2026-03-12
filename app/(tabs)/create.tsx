1b5fcc41a4c8b5b61ca1f1e93e0ec4f00e2b14f2
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-03-12 11:47:34 +0000

    No Image Seleted UI Setup in Create Post

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,4 +1,4 @@
-import { createStyles } from '@/assets/style/create.posr.style';
+import { createStyles } from '@/assets/style/create.post.style';
 import { useUser } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
@@ -9,23 +9,27 @@ import { COLORS } from '../constants/theme';
 export default function CreateScreen() {
 
   const router = useRouter();
-  const {user} =useUser();
+  const { user } = useUser();
 
-  const [caption,setCaption] =useState("");
-  const [seletedImage,setSelectedImage]= useState<string |null >(null);
+  const [caption, setCaption] = useState("");
+  const [seletedImage, setSelectedImage] = useState<string | null>(null);
 
-  if(!seletedImage)
-  {
-    return(
+  if (!seletedImage) {
+    return (
       <View style={createStyles.container}>
         <View style={createStyles.header}>
-          <TouchableOpacity onPress={()=> router.back()}>
+          <TouchableOpacity onPress={() => router.back()}>
             <Ionicons name='arrow-back' size={28} color={COLORS.primary}>
             </Ionicons>
           </TouchableOpacity>
           <Text style={createStyles.headerTitle}>New Post</Text>
-          <View style={{width:28}}></View>
+          <View style={{ width: 28 }}></View>
         </View>
+
+        <TouchableOpacity style={createStyles.emptyImageContainer}>
+          <Ionicons name='image-outline' size={48} color={COLORS.grey}></Ionicons>
+          <Text style={createStyles.emptyImageText}>Tap on select an image</Text>
+        </TouchableOpacity>
       </View>
     )
   }
---
+++ assets/style/create.post.style.ts
@@ -0,0 +1,37 @@
+import { COLORS } from '@/app/constants/theme';
+import { Dimensions, StyleSheet } from 'react-native';
+
+const { width, height } = Dimensions.get("window");
+
+export const createStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+    },
+
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        borderBottomWidth: 0.5,
+        borderBottomColor: COLORS.surface,
+    },
+    headerTitle: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: COLORS.white
+    },
+    emptyImageContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 12,
+    },
+    emptyImageText: {
+        fontSize: 16,
+        color: COLORS.grey,
+
+    }
+});
