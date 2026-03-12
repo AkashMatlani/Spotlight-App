8ddbcc1216e94bed4bf81a5df9a041d6d02bf91a
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-03-12 12:08:07 +0000

    Image Picker func added

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,7 @@
 import { createStyles } from '@/assets/style/create.post.style';
 import { useUser } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from "expo-image-picker";
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
@@ -14,6 +15,20 @@ export default function CreateScreen() {
   const [caption, setCaption] = useState("");
   const [seletedImage, setSelectedImage] = useState<string | null>(null);
 
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync(
+      {
+        mediaTypes: "images",
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 0.8,
+      });
+
+    if (!result.canceled && result.assets.length > 0) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  }
+
   if (!seletedImage) {
     return (
       <View style={createStyles.container}>
@@ -26,13 +41,15 @@ export default function CreateScreen() {
           <View style={{ width: 28 }}></View>
         </View>
 
-        <TouchableOpacity style={createStyles.emptyImageContainer}>
+        <TouchableOpacity style={createStyles.emptyImageContainer} onPress={pickImage}>
           <Ionicons name='image-outline' size={48} color={COLORS.grey}></Ionicons>
           <Text style={createStyles.emptyImageText}>Tap on select an image</Text>
         </TouchableOpacity>
       </View>
     )
   }
+
+
 
   return (
     <View>
