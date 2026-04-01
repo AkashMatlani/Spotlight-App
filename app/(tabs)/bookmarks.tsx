34c8805c6d07251e8bef11b496bcef86f58c8743
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-04-01 13:30:45 +0100

    Bookmarks Data loaded

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,7 +1,9 @@
+import { feedStyles } from '@/assets/style/feed.style'
 import { api } from '@/convex/_generated/api'
 import { useQuery } from 'convex/react'
+import { Image } from 'expo-image'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import Loader from '../components/Loader'
 import { COLORS } from '../constants/theme'
 
@@ -12,8 +14,30 @@ export default function Bookmarks() {
   if (bookamarkPosts === undefined) return <Loader />
   if (bookamarkPosts.length === 0) return <NoBookmarksFound />
   return (
-    <View>
-      <Text>bookmark</Text>
+    <View style={feedStyles.container}>
+      <View style={feedStyles.header}>
+        <Text style={feedStyles.headerTitle}>Bookamarks</Text>
+      </View>
+      <ScrollView contentContainerStyle={{
+        padding: 8,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      }}
+      >
+        {bookamarkPosts.map((post) => {
+          if (!post) return null;
+          return (
+            <View key={post._id} style={{ width: "33.33%", padding: 1 }}>
+              <Image source={post.imageUrl}
+                style={{ width: "100%", aspectRatio: 1 }}
+                contentFit='cover'
+                transition={200}
+                cachePolicy='memory-disk'>
+              </Image>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
@@ -28,6 +52,4 @@ function NoBookmarksFound() {
 
     <Text style={{ color: COLORS.primary, fontSize: 22 }}>No bookmarks post yet</Text>
   </View>
-
-
 }
