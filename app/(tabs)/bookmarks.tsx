4d3177257f9d35f736e3f4faa82b075cf5170a8a
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-04-01 11:32:22 +0100

    No Bookmarks found added

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,10 +1,33 @@
+import { api } from '@/convex/_generated/api'
+import { useQuery } from 'convex/react'
 import React from 'react'
 import { Text, View } from 'react-native'
+import Loader from '../components/Loader'
+import { COLORS } from '../constants/theme'
 
 export default function Bookmarks() {
+
+  const bookamarkPosts = useQuery(api.bookmarks.getBookmarkPosts)
+
+  if (bookamarkPosts === undefined) return <Loader />
+  if (bookamarkPosts.length === 0) return <NoBookmarksFound />
   return (
     <View>
       <Text>bookmark</Text>
     </View>
   )
+}
+
+function NoBookmarksFound() {
+  return <View style={{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.background,
+  }}>
+
+    <Text style={{ color: COLORS.primary, fontSize: 22 }}>No bookmarks post yet</Text>
+  </View>
+
+
 }
--- app/components/post.tsx
+++ app/components/post.tsx
@@ -102,7 +102,6 @@ export default function Post({ post }: PostProps) {
                     )}
             </View>
 
-
             <Image source={post.imageUrl}
                 style={feedStyles.postImage}
                 contentFit='cover'
