91c055447cfc48b942a9de89d93a1cc742203e06
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-04-02 12:11:55 +0100

    No Notification found handled

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,10 +1,29 @@
+import { nofificationStyle } from '@/assets/style/notifications.style'
+import { api } from '@/convex/_generated/api'
+import { Ionicons } from '@expo/vector-icons'
+import { useQuery } from 'convex/react'
 import React from 'react'
 import { Text, View } from 'react-native'
+import Loader from '../components/Loader'
+import { COLORS } from '../constants/theme'
 
 export default function notifications() {
+
+  const notifications = useQuery(api.notifications.getNotifications)
+
+  if (notifications === undefined) return <Loader />
+
+  if (notifications.length === 0) return <NoNotificationFound />
   return (
     <View>
       <Text>notifications</Text>
     </View>
   )
+}
+
+function NoNotificationFound() {
+  return <View style={[nofificationStyle.container, nofificationStyle.centerd]}>
+    <Ionicons name='notifications-outline' size={48} color={COLORS.primary} />
+    <Text style={{ fontSize: 20, color: COLORS.white }}>No notifications yet</Text>
+  </View>
 }
---
+++ assets/style/notifications.style.ts
@@ -0,0 +1,15 @@
+
+import { COLORS } from "@/app/constants/theme";
+import { StyleSheet } from "react-native";
+export const nofificationStyle = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+    },
+    centerd: {
+        justifyContent: "center",
+        alignItems: "center",
+    }
+
+})
